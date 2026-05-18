b9224e3c9398e79900014336d6c6144862372545
Rizal <rizalsofiana1976@gmail.com>
2026-05-19 01:25:11 +0700

    update major UI. TODO: fix logo size in dashboard

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,2 +1,2 @@
 <a
-    {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-black hover:bg-gray-200 focus:outline-none focus:bg-emerald-700 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+    {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-black hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-white" />
+                        <x-application-logo class="w-10 h-10 block fill-current text-white" />
                     </a>
                 </div>
 
