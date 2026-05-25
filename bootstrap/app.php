ee1e114e56dcdba1bae7587d2cfc116ab7aab019
Rizal <rizalsofiana1976@gmail.com>
2026-05-26 00:44:44 +0700

    registry middleware check role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\checkRole::class, // Sesuaikan dengan nama file & namespace middleware role yang Anda buat
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
